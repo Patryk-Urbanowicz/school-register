998fe2cc66b19cd612f5e78fe2373189b0032f2f
szymonorz <originalemail001@gmail.com>
2022-05-25 14:22:17 +0200

    Vaadin: Make the "Hours:" column sticky.

--- app/src/main/java/pl/school/register/view/components/ColumnLabel.java
+++ app/src/main/java/pl/school/register/view/components/ColumnLabel.java
@@ -6,7 +6,15 @@ import com.vaadin.flow.component.html.*;
 @Tag(value = "th")
 public class ColumnLabel extends RowSegment {
 	public ColumnLabel(String label){
-		setClassName("column-label");
+		if (label.toLowerCase().contains("hours")){
+			setClassName("hour-segment");
+			addClassName("column-label");
+			/* When scrolling down make the "Hours:" display over the hour-segment*/
+			getStyle().set("z-index", "11");
+		}else{
+			setClassName("column-label");
+		}
+
 		Label weekdayLabel = new Label(label);
 		add(weekdayLabel);
 	}
