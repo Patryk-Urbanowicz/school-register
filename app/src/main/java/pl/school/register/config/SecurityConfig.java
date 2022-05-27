352079ccecd501b29e3540b04c0c37595c74df10
szymonorz <originalemail001@gmail.com>
2022-05-27 22:54:05 +0200

    Security: Add temporary InMemoryUserDetailsManager

--- app/src/main/java/pl/school/register/config/SecurityConfig.java
+++ app/src/main/java/pl/school/register/config/SecurityConfig.java
@@ -3,10 +3,16 @@ package pl.school.register.config;
 import com.vaadin.flow.spring.security.VaadinWebSecurityConfigurerAdapter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import pl.school.register.view.LoginLayout;
 
 @EnableWebSecurity
 public class SecurityConfig {
@@ -27,15 +33,39 @@ public class SecurityConfig {
     class VaadinSecurityConfig extends VaadinWebSecurityConfigurerAdapter{
         @Override
         protected void configure(HttpSecurity http) throws Exception {
-            http.csrf().disable()
-                    .antMatcher("/**")
-                    .authorizeRequests()
-                    .anyRequest().permitAll();
+            super.configure(http);
+            http.antMatcher("/**");
+            setLoginView(http, LoginLayout.class);
         }
 
         @Override
         public void configure(WebSecurity web) throws Exception {
             web.ignoring().antMatchers("/images/**");
+        }
+
+        @Override
+        protected UserDetailsService userDetailsService() {
+            UserDetails student = User
+                    .withUsername("joe")
+                    .password("{noop}12345678")
+                    .roles("STUDENT")
+                    .build();
+            UserDetails parent = User
+                    .withUsername("maria")
+                    .password("{noop}joethebest")
+                    .roles("PARENT")
+                    .build();
+            UserDetails teacher = User
+                    .withUsername("andrew")
+                    .password("{noop}renatixon")
+                    .roles("TEACHER")
+                    .build();
+            return new InMemoryUserDetailsManager(student, parent, teacher);
+        }
+
+        @Override
+        protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+            auth.userDetailsService(userDetailsService());
         }
     }
 }
