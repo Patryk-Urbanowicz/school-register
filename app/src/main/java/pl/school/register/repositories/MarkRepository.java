34236c235bf60ef50da29136ad020c674875b0f9
kamilex00 <kamil.kocieniewski7@gmail.com>
2022-07-12 21:01:28 +0200

    Added weighted average for each subject for student

--- app/src/main/java/pl/school/register/repositories/MarkRepository.java
+++ app/src/main/java/pl/school/register/repositories/MarkRepository.java
@@ -7,6 +7,7 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import pl.school.register.model.Lesson;
 import pl.school.register.model.Mark;
+import pl.school.register.model.SubjectAvgPair;
 
 import java.util.List;
 import java.util.Optional;
@@ -21,5 +22,17 @@ public interface MarkRepository extends JpaRepository<Mark, Long> {
     @Query(value = "SELECT \n" +
             "    cast(sum(m.value * m.weight) AS float) / sum(m.weight) as w_avg \n" +
             "FROM mark AS m WHERE student_id=:student_id AND lesson_id=:lesson_id", nativeQuery = true)
-    Optional<Float> findWeightedAverageByStudentIdAndLessonId(@Param("student_id") Long student_id, @Param("lesson_id") Long lesson_id);
+    Optional<Float> findWeightedAverageByStudentIdAndLessonId(@Param("student_id") Long student_id,
+                                                              @Param("lesson_id") Long lesson_id);
+
+    @Query(value = "SELECT \n" +
+            "    s.subject_name, \n" +
+            "    cast(sum(m.value * m.weight) AS float) / sum(m.weight) as w_avg\n" +
+            "FROM lesson JOIN mark m on lesson.id = m.lesson_id\n" +
+            "            JOIN subject s on lesson.subject_id = s.id\n" +
+            "                  WHERE school_class_id = :school_class_id AND m.student_id = :student_id " +
+            "group by s.subject_name", nativeQuery = true)
+    List<SubjectAvgPair> findAllWeightedAverageForStudentGroupingBySubject(
+            @Param("school_class_id") Long school_class_id,
+            @Param("student_id") Long student_id);
 }
--- app/src/main/java/pl/school/register/service/MarkService.java
+++ app/src/main/java/pl/school/register/service/MarkService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestBody;
 import pl.school.register.model.Mark;
+import pl.school.register.model.SubjectAvgPair;
 import pl.school.register.repositories.MarkRepository;
 
 import java.util.List;
@@ -42,5 +43,9 @@ public class MarkService {
         Optional<Float> chck = markRepository.findWeightedAverageByStudentIdAndLessonId(student_id, lesson_id);
         if(chck.isEmpty()) throw new Exception("Couldn't estimate the average");
         return chck.get();
+    }
+
+    public List<SubjectAvgPair> getAllWeightedAverageForStudentGroupingBySubject(Long school_class_id, Long student_id) {
+        return markRepository.findAllWeightedAverageForStudentGroupingBySubject(school_class_id, student_id);
     }
 }
