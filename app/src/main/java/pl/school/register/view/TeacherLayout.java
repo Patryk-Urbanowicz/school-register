0f066e2bdbe7c5d1cd3976abf0803c11ac5d7954
szymonorz <originalemail001@gmail.com>
2022-07-17 22:55:02 +0200

    Fix issues after deleting Student_Class.

--- app/src/main/java/pl/school/register/view/TeacherLayout.java
+++ app/src/main/java/pl/school/register/view/TeacherLayout.java
@@ -17,44 +17,44 @@ import pl.school.register.view.components.TeacherNavBar;
 
 import java.util.*;
 
-//@Route(value = "teacher")
-//@CssImport(value = "./themes/accordion.css", themeFor = "vaadin-accordion-panel")
-//public class TeacherLayout extends AppLayout {
-//    private SchoolClassService schoolClassService;
-//    public TeacherLayout(SchoolClassService schoolClassService) {
-//        this.schoolClassService = schoolClassService;
-//        Teacher teacher = new Teacher();
-//        teacher.setFirstName("aaa");
-//        teacher.setLastName("aAAAAA");
-//        addToDrawer(getDrawer());
-//        addToNavbar(new TeacherNavBar(teacher));
-//    }
-//
-//    private VerticalLayout getDrawer() {
-//        VerticalLayout links = new VerticalLayout();
-//        links.setHeightFull();
-//        links.setClassName("teacher-drawer");
-//        List<SchoolClass> classes = schoolClassService.getAllByTeacherWhoHasLessonsWith(1L);
-//        classes.forEach(_class -> {
-//            Accordion accordion = new Accordion();
-//            accordion.setClassName("accordion");
-//            VerticalLayout accordionLinks = new VerticalLayout();
-//            RouterLink listOfStudents = new RouterLink("Students",
-//                                    TeacherSchoolClassStudentListView.class,
-//                                    new RouteParameters(Map.of("classId", _class.getId().toString())));
-//            accordionLinks.add(listOfStudents);
-//            _class.getLessons().forEach(lesson -> {
-//                Map<String, String> params = new HashMap<>();
-//                params.put("classId", lesson.getSchoolClass().getId().toString());
-//                params.put("subjectId", lesson.getSubject().getId().toString());
-//                RouterLink link = new RouterLink(lesson.getSubject().getSubjectName(),
-//                        TeacherSchoolClassView.class,
-//                        new RouteParameters(params));
-//                accordionLinks.add(link);
-//            });
-//            accordion.add(_class.getClassName(), accordionLinks);
-//            links.add(accordion);
-//        });
-//        return links;
-//    }
-//}
+@Route(value = "teacher")
+@CssImport(value = "./themes/accordion.css", themeFor = "vaadin-accordion-panel")
+public class TeacherLayout extends AppLayout {
+    private SchoolClassService schoolClassService;
+    public TeacherLayout(SchoolClassService schoolClassService) {
+        this.schoolClassService = schoolClassService;
+        Teacher teacher = new Teacher();
+        teacher.setFirstName("aaa");
+        teacher.setLastName("aAAAAA");
+        addToDrawer(getDrawer());
+        addToNavbar(new TeacherNavBar(teacher));
+    }
+
+    private VerticalLayout getDrawer() {
+        VerticalLayout links = new VerticalLayout();
+        links.setHeightFull();
+        links.setClassName("teacher-drawer");
+        List<SchoolClass> classes = schoolClassService.getAllByTeacherWhoHasLessonsWith(1L);
+        classes.forEach(_class -> {
+            Accordion accordion = new Accordion();
+            accordion.setClassName("accordion");
+            VerticalLayout accordionLinks = new VerticalLayout();
+            RouterLink listOfStudents = new RouterLink("Students",
+                                    TeacherSchoolClassStudentListView.class,
+                                    new RouteParameters(Map.of("classId", _class.getId().toString())));
+            accordionLinks.add(listOfStudents);
+            _class.getLessons().forEach(lesson -> {
+                Map<String, String> params = new HashMap<>();
+                params.put("classId", lesson.getSchoolClass().getId().toString());
+                params.put("subjectId", lesson.getSubject().getId().toString());
+                RouterLink link = new RouterLink(lesson.getSubject().getSubjectName(),
+                        TeacherSchoolClassView.class,
+                        new RouteParameters(params));
+                accordionLinks.add(link);
+            });
+            accordion.add(_class.getClassName(), accordionLinks);
+            links.add(accordion);
+        });
+        return links;
+    }
+}
--- app/src/main/java/pl/school/register/view/TeacherSchoolClassView.java
+++ app/src/main/java/pl/school/register/view/TeacherSchoolClassView.java
@@ -1,73 +1,73 @@
-//package pl.school.register.view;
-//
-//import com.vaadin.flow.component.html.H1;
-//import com.vaadin.flow.component.orderedlayout.VerticalLayout;
-//import com.vaadin.flow.router.*;
-//import pl.school.register.model.*;
-//import pl.school.register.model.projections.MeetingInWeek;
-//import pl.school.register.service.MeetingService;
-//import pl.school.register.view.components.ResponsiveTableWrapper;
-//import pl.school.register.view.components.ScheduleTable;
-//
-//import java.time.LocalDate;
-//import java.time.LocalDateTime;
-//import java.time.format.DateTimeFormatter;
-//import java.time.temporal.TemporalField;
-//import java.time.temporal.WeekFields;
-//import java.util.*;
-//import java.util.stream.Collectors;
-//
-//@Route(value = "teacher/class/:classId/subject/:subjectId", layout = TeacherLayout.class)
-//public class TeacherSchoolClassView extends VerticalLayout implements BeforeEnterObserver {
-//    private SchoolClass schoolClass;
-//    private MeetingService meetingService;
-//    public TeacherSchoolClassView(MeetingService meetingService){
-//        this.meetingService = meetingService;
-//        setClassName("school-class-view");
-//        removeAll();
-//    }
-//
-//    @Override
-//    public void beforeEnter(BeforeEnterEvent beforeEnterEvent) {
-//        removeAll(); //A very bad way of "refreshing" components on page
-//
-//        RouteParameters params = beforeEnterEvent.getRouteParameters();
-//        Optional<String> subjectId = params.get("subjectId");
-//        Optional<String> classId = params.get("classId");
-//        if (subjectId.isPresent() && classId.isPresent()){
-//            LocalDate now = LocalDate.now();
-//            TemporalField fieldISO = WeekFields.of(Locale.GERMANY).dayOfWeek();
-//            System.out.println(now.with(fieldISO, 1));
-//            System.out.println(now.with(fieldISO, 5));
-//
-//            LocalDate monday = now.with(fieldISO, 1);
-//            LocalDate friday = now.with(fieldISO, 5);
-//
-//            Long subjectIdL = Long.parseLong(subjectId.get());
-//            Long classIdL = Long.parseLong(classId.get());
-//            List<MeetingInWeek> meetingsInWeek = meetingService
-//                    .getWithWeekDayByTeacherIdAndSchoolClassId(1L, classIdL, subjectIdL,
-//                                                                monday, friday);
-//            DateTimeFormatter dtf = DateTimeFormatter.ofPattern("HH:mm");
-//            List<LessonBlock> blocks = meetingsInWeek.stream().map(m -> {
-//                LessonBlock lb = new LessonBlock();
-//                Subject subject = new Subject();
-//                subject.setSubjectName(m.getSubjectName());
-//                Lesson l = new Lesson();
-//                Teacher teacher = new Teacher();
-//                teacher.setFirstName(m.getTeacherFirstName());
-//                teacher.setLastName(m.getTeacherLastName());
-//                l.setTeacher(teacher);
-//                lb.setWeekDay(m.getWeekDay());
-//                LocalDateTime time = m.getLessonStartTime();
-//                lb.setStartTime(time.format(dtf));
-//                lb.setLesson(l);
-//                return lb;
-//            }).collect(Collectors.toList());
-//
-//            add(new H1("Class: " + classIdL +  " Subject: " + subjectIdL ));
-//            add(new ResponsiveTableWrapper(new ScheduleTable(blocks)));
-//        }
-//
-//    }
-//}
+package pl.school.register.view;
+
+import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.*;
+import pl.school.register.model.*;
+import pl.school.register.model.projections.MeetingInWeek;
+import pl.school.register.service.MeetingService;
+import pl.school.register.view.components.ResponsiveTableWrapper;
+import pl.school.register.view.components.ScheduleTable;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.TemporalField;
+import java.time.temporal.WeekFields;
+import java.util.*;
+import java.util.stream.Collectors;
+
+@Route(value = "teacher/class/:classId/subject/:subjectId", layout = TeacherLayout.class)
+public class TeacherSchoolClassView extends VerticalLayout implements BeforeEnterObserver {
+    private SchoolClass schoolClass;
+    private MeetingService meetingService;
+    public TeacherSchoolClassView(MeetingService meetingService){
+        this.meetingService = meetingService;
+        setClassName("school-class-view");
+        removeAll();
+    }
+
+    @Override
+    public void beforeEnter(BeforeEnterEvent beforeEnterEvent) {
+        removeAll(); //A very bad way of "refreshing" components on page
+
+        RouteParameters params = beforeEnterEvent.getRouteParameters();
+        Optional<String> subjectId = params.get("subjectId");
+        Optional<String> classId = params.get("classId");
+        if (subjectId.isPresent() && classId.isPresent()){
+            LocalDate now = LocalDate.now();
+            TemporalField fieldISO = WeekFields.of(Locale.GERMANY).dayOfWeek();
+            System.out.println(now.with(fieldISO, 1));
+            System.out.println(now.with(fieldISO, 5));
+
+            LocalDate monday = now.with(fieldISO, 1);
+            LocalDate friday = now.with(fieldISO, 5);
+
+            Long subjectIdL = Long.parseLong(subjectId.get());
+            Long classIdL = Long.parseLong(classId.get());
+            List<MeetingInWeek> meetingsInWeek = meetingService
+                    .getWithWeekDayByTeacherIdAndSchoolClassId(1L, classIdL, subjectIdL,
+                                                                monday, friday);
+            DateTimeFormatter dtf = DateTimeFormatter.ofPattern("HH:mm");
+            List<LessonBlock> blocks = meetingsInWeek.stream().map(m -> {
+                LessonBlock lb = new LessonBlock();
+                Subject subject = new Subject();
+                subject.setSubjectName(m.getSubjectName());
+                Lesson l = new Lesson();
+                Teacher teacher = new Teacher();
+                teacher.setFirstName(m.getTeacherFirstName());
+                teacher.setLastName(m.getTeacherLastName());
+                l.setTeacher(teacher);
+                lb.setWeekDay(m.getWeekDay());
+                LocalDateTime time = m.getLessonStartTime();
+                lb.setStartTime(time.format(dtf));
+                lb.setLesson(l);
+                return lb;
+            }).collect(Collectors.toList());
+
+            add(new H1("Class: " + classIdL +  " Subject: " + subjectIdL ));
+            add(new ResponsiveTableWrapper(new ScheduleTable(blocks)));
+        }
+
+    }
+}
